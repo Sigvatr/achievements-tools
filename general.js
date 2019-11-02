ac363ec74ebd2b564cfafdbcaa1f42fa6df95672
Sigvatr <sigvatr@wp.pl>
2019-11-02 17:10:49 +0100

    Addding the two helper functions of general purpuse

--- general.js
+++ general.js
@@ -1,17 +1,16 @@
 // Open you Steam profile page with all achievements for you
 // Click "View global achievement stats" for more data
 // Run following console
-
 Array.from(document.getElementsByClassName('achieveRow'))
 	.map(row => {
 		const descriptionTag = row.getElementsByTagName('h5');
 		return {
-			img: row.querySelector('div.achieveImgHolder img').src,
+			img: row.querySelector('div.achieveImgHolder img').src.trim(),
 			unlocked: row.classList.length > 1 && row.classList[1] === 'unlocked',
-			percentage: row.getElementsByClassName('achievePercent')[0].innerText,
-			name: row.getElementsByTagName('h3')[0].innerText,
-			description: descriptionTag ? descriptionTag[0].innerText : null
-		}
+			percentage: row.getElementsByClassName('achievePercent')[0].innerText.trim(),
+			name: row.getElementsByTagName('h3')[0].innerText.trim(),
+			description: descriptionTag ? descriptionTag[0].innerText.trim() : null
+		};
 	});
 
 // You recieve collection of that objects:
@@ -25,4 +24,24 @@ Array.from(document.getElementsByClassName('achieveRow'))
 	}
 */
 // you can use the map function
+
+
+function toCSVRow(datum, columns = null) {
+	return (columns || Object.keys(datum))
+		.map(column => {
+			if (typeof datum[column] === "string") {
+				return '"' + datum[column].replace(/\"/g, '""') + '"';
+			}
+
+			return datum[column];
+		})
+		.join(',');
+}
+
+
+function displayOnWholePage(text) {
+	document
+		.getElementsByTagName('body')[0]
+		.innerHTML = `<pre>${text}</pre>`;
+}
 
