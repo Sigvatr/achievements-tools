f2de1286ea22b51f51ea9f69e6e999bb718f758c
Sigvatr <sigvatr@wp.pl>
2019-11-02 15:51:12 +0100

    New tool for Civilization VI

--- Sid Meier’s Civilization VI.js
+++ Sid Meier’s Civilization VI.js
@@ -28,3 +28,46 @@ Array.from(document.getElementsByClassName('subSection'))
 }
 */
 // you can use the map function
+
+
+// Getting achievment list with DLC which is used by it
+// https://civilization.fandom.com/wiki/Steam_achievements_in_Civ6
+
+Array.from(document.getElementById('mw-content-text').children)
+	.reduce(
+		function(actual, item) {
+			if (item.nodeName === 'TABLE' && item.className === "steam-achievement-container") {
+				actual.results.push({
+					name: item.getElementsByClassName('steam-achievement-name')[0].textContent.trim(),
+					dlc: actual.header + (!actual.subHeader ? '' : ': ' + actual.subHeader),
+					note: item.getElementsByTagName('dd')[0].textContent.trim()
+				});
+			}
+
+			if (item.nodeName === 'H2') {
+				actual.header = item.getElementsByTagName('span')[0].textContent.trim();
+				actual.subHeader = null;
+			}
+
+			if (item.nodeName === 'H3') {
+				actual.subHeader = item.getElementsByTagName('span')[0].textContent.trim();
+			}
+
+			return actual;
+		},
+		{
+			header: null,
+			subHeader: null,
+			results: []
+		}
+	)
+	.results
+
+// You recieve collection of that objects:
+/*
+{
+	dlc: "Achievements in the base game"
+	name: "100th Anniversary"
+	note: "2016, the year that Civilization VI was released, was the 100th anniversary of the National Park Service, which manages national parks in the United States."
+}
+*/
